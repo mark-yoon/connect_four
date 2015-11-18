cd211f35d5f8dc9663beba51508d9b86e8ec0e33
Mark Yoon <mark.youngho.yoon@gmail.com>
2015-11-18 01:10:21 -0500

    Added all game state logic, need to add player moves

--- game.py
+++ game.py
@@ -13,23 +13,60 @@ class Game(object):
   def init(self):
     print "Welcome to CS 4701 Connect 4!"
     print "Please refer to Google regarding the rules of this game."
-    entity = raw_input("Would you like player 1 to be a computer? [Y/n]")
-    while player1 == None:
+    valid = False
+    while not valid:
+      entity = raw_input("Would you like player 1 to be a computer? [Y/n]")
       if entity.lower() == 'y':
         player1 = AI('o')
+        valid = True
       elif entity.lower() == 'n':
         player1 = Human('o')
+        valid = True
       else:
         print "Please input a valid option."
-        entity = raw_input("Would you like player 1 to be a computer? [Y/n]")
 
     print "Great!"
-    entity2 = raw_input("Would you like player 2 to be a computer? [Y/n]")
-    while entity2 == None:
-      if entity2.lower() == 'y':
+
+    valid = False
+    while not valid:
+      entity = raw_input("Would you like player 2 to be a computer? [Y/n]")
+      if entity.lower() == 'y':
         player2 = AI('x')
-      elif entity2.lower() == 'n':
+        valid = True
+      elif entity.lower() == 'n':
         player2 = Human('x')
+        valid = True
       else:
         print "Please input a valid option."
-        entity2 = raw_input("Would you like player 1 to be a computer? [Y/n]")
+
+    complete = false
+    while not complete:
+      outcome = self.play_game()
+      valid = False
+      while not valid:
+        again = raw_input("Would you like to play again? [Y/n]")
+        if again.lower() == 'y':
+          valid = True
+        elif again.lower() == 'n':
+          print "Thank you for playing!"
+          return
+        else:
+          print "Please input a valid option."
+
+
+  def play_game(self):
+    complete = false
+    while not complete:
+      print "Player 1's turn!"
+      player1.nextmove(board)
+      print board
+      if board.check_win(1):
+        print "Player 1 wins!"
+        return 1
+
+      print "Player 2's turn!"
+      player2.nextmove(board)
+      print board
+      if board.check_win(2):
+        print "Player 2 wins!"
+        return 2
