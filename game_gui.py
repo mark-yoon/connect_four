3ee38b7df1171849370000875b56ea5b93463c82
Mark Yoon <mark.youngho.yoon@gmail.com>
2015-12-08 21:12:24 -0500

    Made grid

--- game_gui.py
+++ game_gui.py
@@ -3,97 +3,89 @@ from human import *
 from board import *
 import pygame
 
-WHITE = (255,255, 255)
-
-class Game(object):
-  """
-  Game object that holds the state of the board and players' moves
-  """
-
-  board = None
-  player1 = None
-  player2 = None
-  winner = None
-  turn_count = None
-
-  def __init__(self):
-    pygame.init()
-    size = (800,600)
-    screen = pygame.display.set_mode(size)
-    screen.fill(WHITE)
-    pygame.display.set_caption("Connect 4")
-    running = True
-
-    while running: 
-      for event in pygame.event.get():
-        pass
 
 
-    print "Welcome to CS 4701 Connect 4!"
-    print "Please refer to Google regarding the rules of this game."
-    valid = False
-    while not valid:
-      entity = raw_input("Would you like player 1 to be a computer? [Y/n]")
-      if entity.lower() == 'y':
-        self.player1 = AI()
-        valid = True
-      elif entity.lower() == 'n':
-        self.player1 = Human(1)
-        valid = True
-      else:
-        print "Please input a valid option."
+  # def play_game(self):
+  #   complete = False
+  #   while not complete:
+  #     print "Player 1's turn!"
+  #     self.player1.next_move(self.board)
+  #     print self.board
+  #     print self.board.check_win(1)
+  #     if self.board.check_win(1)[0]:
+  #       print "Player 1 wins!"
+  #       return 1
 
-    print "Great!"
+  #     print "Player 2's turn!"
+  #     self.player2.next_move(self.board)
+  #     print self.board
+  #     if self.board.check_win(2)[0]:
+  #       print "Player 2 wins!"
+  #       return 2
 
-    valid = False
-    while not valid:
-      entity = raw_input("Would you like player 2 to be a computer? [Y/n]")
-      if entity.lower() == 'y':
-        self.player2 = AI()
-        valid = True
-      elif entity.lower() == 'n':
-        self.player2 = Human(2)
-        valid = True
-      else:
-        print "Please input a valid option."
+# ==============================================================================================================
 
-    print "Great!"
+white = (255,255,255)
+black = (0,0,0)
 
-    complete = False
-    while not complete:
-      self.board = Board()
-      print self.board
-      outcome = self.play_game()
-      valid = False
-      while not valid:
-        again = raw_input("Would you like to play again? [Y/n]")
-        if again.lower() == 'y':
-          valid = True
-        elif again.lower() == 'n':
-          print "Thank you for playing!"
-          return
-        else:
-          print "Please input a valid option."
+pygame.init()
+size = (800,700)
+screen = pygame.display.set_mode(size)
+screen.fill(white)
+pygame.display.set_caption("Connect 4")
+running = True
+
+game_state = 0
+
+def create_text(screen, font_path, size, text, color, center):
+  font = pygame.font.Font(font_path, size)
+  text = font.render(text, 0, color)
+  textpos = text.get_rect()
+  textpos.centerx = center[0]
+  textpos.centery = center[1]
+  screen.blit(text, textpos)
+
+def create_rect(screen, rect, color, center, thickness):
+  rectangle = pygame.Rect(rect)
+  rectangle.center = center
+  pygame.draw.rect(screen, color, rectangle, thickness)
+  return rectangle
+
+player1 = Human(1)
+player2 = Human(2)
+board = Board()
+
+while running: 
+  screen.fill(white)
+  if game_state == 0:
+    play_rect = create_rect(screen, [10, 10, 200, 100], black, (400, 350), 1)
+
+    create_text(screen, "font/mario.ttf", 25, "PLAY", black, screen.get_rect().center)
+    create_text(screen, "font/mario.ttf", 45, "Welcome to Connect 4!", black, (screen.get_rect().centerx, screen.get_rect().centery -200))
+
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+      elif event.type == pygame.MOUSEBUTTONDOWN:
+        if play_rect.collidepoint(event.pos):
+          game_state = 1
+      pygame.display.update()
 
 
-  def play_game(self):
-    complete = False
-    while not complete:
-      print "Player 1's turn!"
-      self.player1.next_move(self.board)
-      print self.board
-      print self.board.check_win(1)
-      if self.board.check_win(1)[0]:
-        print "Player 1 wins!"
-        return 1
+  if game_state == 1:
 
-      print "Player 2's turn!"
-      self.player2.next_move(self.board)
-      print self.board
-      if self.board.check_win(2)[0]:
-        print "Player 2 wins!"
-        return 2
+    x_center = 0
+    y_center = 0
 
+    for i in range(0, 7):
+      x_center += 100
+      y_center = 0
+      for j in range(0, 6):
+        y_center += 100
+        print "x center: " + str(x_center)
+        create_rect(screen, [10, 10, 100, 100], black, (x_center, y_center), 1)
 
-if __name__ == "__main__":
-    game = Game()
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+    pygame.display.update()
